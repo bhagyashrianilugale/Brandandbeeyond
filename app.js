519b3712e70c15e745d1cd572191a9eab8a48b6e
bhagyashriugale <bhagyashriugale6970@gmail.com>
2024-08-08 05:52:57 +0530

    Update

--- app.js
+++ app.js
@@ -8,3 +8,12 @@ gsap.to(
     
 );
 
+let sideMenu = document.getElementById("sidemenu");
+
+function openMenu(){
+  sideMenu.style.right = "0";
+}
+
+function closeMenu(){
+  sideMenu.style.right = "-200px";
+}
